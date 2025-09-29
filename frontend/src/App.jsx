ef156410fe4bca1444cfd32ec8854bb168b975f6
johnnydanny <coderade9@gmail.com>
2025-09-29 22:24:55 +0100

    make it production ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/todos";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export default function App() {
   const [todos, setTodos] = useState([]);
