799e6c520f376bdc012bc81747386b49ec0a2983
johnnydanny <coderade9@gmail.com>
2025-09-29 23:05:10 +0100

    fix config issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = `${import.meta.env.VITE_API_URL}/todos`;
 
 export default function App() {
   const [todos, setTodos] = useState([]);
